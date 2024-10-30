e47f5f5a0227d8313ca58b290fbe2ab543d84819
minhazapon <minhajulabedinapon@gmail.com>
2024-10-30 07:29:51 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+
+
+const num: number = 1;
+const num2: number = 2;
+const all = num + num2
+
+console.log(all)
+
+
+function Add( a: number, b: number ){
+
+   const allAdd = a + b 
+   console.log(allAdd)
+
+}
+
+Add(1, 30)  
+ 
+
+ 
