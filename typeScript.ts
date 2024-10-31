e6bcca5e304ddcd0e7c09c715cb55c778f5cb814
minhazapon <minhajulabedinapon@gmail.com>
2024-10-31 01:52:25 -0700

    added file

---
+++ typeScript.ts
@@ -0,0 +1,71 @@
+{
+
+
+    //////////////basic typeScript//////////////////
+      
+
+    let countryName: string = 'banladesh, india, pakistan, afganistan' 
+
+    console.log(countryName)
+
+    let yourNumber: number = 1950374409 
+
+    console.log(yourNumber)
+
+    let aponIspythonDeveloper: boolean = false 
+
+    console.log(aponIspythonDeveloper)
+
+    let mangoIshere: null = null 
+
+    console.log(mangoIshere)
+
+    let appleIsHere: undefined = undefined 
+
+    console.log(appleIsHere)
+
+
+    //////array in typeScript ////////
+
+
+    let skills: string[] = [ 'js', 'ts', 'react', 'next.js', 'redux', 'express', 'mongoose' ]
+
+    console.log(skills)
+
+    let someNum: number[] = [ 23, 45, 35, 3545, 355, 455, 455, 5355, 35, 355, 355 ]
+
+    console.log(someNum)
+
+
+
+    ///////tuple in type/////////
+
+
+    let allThinks: (string | number)[] = [ 'bangladesh', 'india', 'kashmir', 'bhutan', 34, 354, 454, 345, 455] 
+    
+    console.log(allThinks)
+
+
+
+    //////////////basic typeScript//////////////////
+
+
+
+    
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
