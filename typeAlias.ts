b282373b63ab46dd55ca0c07d233ef4a570cb700
minhazapon <minhajulabedinapon@gmail.com>
2024-10-30 20:43:31 -0700

    type alias file

---
+++ typeAlias.ts
@@ -0,0 +1,28 @@
+{
+
+ 
+
+    type allValue = string | number 
+    
+
+    const name: allValue = 'minhazul abedin apon';
+    const number: allValue = 1950374409 
+    const location: allValue = 'bangladesh'
+    const id: allValue = 101
+    const allThik = { name, number, location, id } 
+
+    console.log(allThik)
+
+
+    function valZ( a: allValue , b: allValue, c: allValue ){
+
+         const allV  = { a, b, c }
+         console.log(allV)
+
+    }
+
+    valZ( 12, 'apon', '40' )
+
+
+
+}
